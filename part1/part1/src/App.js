078b00a14160ef6e785e40b4c2a556ff3a790682
Fxizenta <1193295835@qq.com>
2021-04-13 19:10:10 +0800

    add new file

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+const Hello = (props) => {
+    return (
+        <div>
+            <p>Hello {props.name},you are {props.age} years old</p>
+        </div>
+    )
 }
 
-export default App;
+const Test = (props) => {
+    return (
+        <div>
+            <p>Hello world{props.name}</p>
+        </div>
+    )
+}
+const App = () => {
+    const name = 'Peter';
+    const age = 10;
+    return (
+        <div>
+            <h1>Greetings</h1>
+            <Hello name="mjj" age={17+3} />
+            <Hello name={name} age={age}/>
+            <Test name="123"/>
+        </div>
+    )
+}
+
+export default App
