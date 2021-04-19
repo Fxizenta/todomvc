84f78b9286e29f1ca5d5a357bf217601f3c90d8d
Fxizenta <1193295835@qq.com>
2021-04-19 10:17:23 +0800

    change file

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,14 +2,15 @@ import React, { useState } from 'react'
 import Person from "./components/Person";
 const App = () => {
     const [ persons, setPersons ] = useState([
-        { name: 'Arto Hellas' }
+        { name: 'Arto Hellas', phone: 23323332333 }
     ])
     const [ newName, setNewName ] = useState('')
-
+    const [newPhone,setNewPhone] = useState()
     const addPerson = (event) =>{
         event.preventDefault()
         const personObject = {
-            name: newName
+            name: newName,
+            phone: newPhone
         }
         if(persons.filter(person=>person.name===personObject.name).length>0) {
             window.confirm(`${personObject.name} is already added to phonebook`)
@@ -24,6 +25,9 @@ const App = () => {
     const handlePersonChange = (event) => {
         setNewName(event.target.value)
     }
+    const handlePhoneChange = (event) => {
+        setNewPhone(event.target.value)
+    }
     return (
         <div>
             <h2>Phonebook</h2>
@@ -31,14 +35,17 @@ const App = () => {
                 <div>
                     name: <input value={newName} onChange={handlePersonChange}/>
                 </div>
+                <div>number: <input value={newPhone} onChange={handlePhoneChange}/></div>
                 <div>
                     <button type="submit">add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
-            <ul>
-            {persons.map(person => <Person key={person.name} person={person}/>)}
-            </ul>
+            <div>
+                <ul>
+                {persons.map(person => <Person key={person.name} person={person}/>)}
+                </ul>
+            </div>
             ...
         </div>
     )
