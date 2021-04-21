ec8caf372c0e8e82fcf3e524a831e63832e3ab84
Fxizenta <1193295835@qq.com>
2021-04-21 10:46:56 +0800

    change file

--- part2/restcountries/src/components/Countries.js
+++ part2/restcountries/src/components/Countries.js
@@ -1,16 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import Country from "./Country";
 
 const Countries = ({countries, newSearch}) => {
+    const [showCountry,setShowContry] = useState();
+
     const show = event => {
         console.log(event.target.value);
         const cont = countries.filter(country =>
-            country.name.includes(event.target.value))
+            country.name.includes(event.target.value)
+        );
+        console.log("cont: ", cont);
+        setShowContry(cont[0]);
     };
+
 
     const entries = countries.filter(country => country.name.toUpperCase().includes(newSearch.toUpperCase()))
     if (entries.length >= 10) {
         return <p>Too many matches, specify another filter</p>
+    }
+
+    if(entries.length===1){
+        return (
+            <ul>
+                {countries
+                    .filter(country =>
+                        country.name.toUpperCase().includes(newSearch.toUpperCase())
+                    )
+                    .map(country => (
+                        <Country
+                            key={country.name}
+                            name={country.name}
+                            capital={country.capital}
+                            population={country.population}
+                            languages={country.languages}
+                            flagUrl={country.flag}
+                        />
+                    ))}
+            </ul>
+
+        )
     }
     return (
         <ul>
@@ -22,10 +50,10 @@ const Countries = ({countries, newSearch}) => {
                     <Country
                         key={country.name}
                         name={country.name}
-                        capital={country.capital}
-                        population={country.population}
-                        languages={country.languages}
-                        flagUrl={country.flag}
+                        //capital={country.capital}
+                        //population={country.population}
+                        //languages={country.languages}
+                        //flagUrl={country.flag}
                     />
             ))}
         </ul>
--- part2/restcountries/src/components/Country.js
+++ part2/restcountries/src/components/Country.js
@@ -1,9 +1,25 @@
 import React from "react";
 
-const Country = ({name}) => {
+const Country = ({name,capital,population,languages,flagUrl}) => {
+    if(population!=undefined) {
+        return (
+            <div>
+                <h1>{name}</h1>
+                <p>capital {capital}</p>
+                <p>population {population}</p>
+                <h3>languages</h3>
+                <ul>
+                    {languages.map(language => (
+                        <li key={language.name}>{language.name}</li>
+                    ))}
+                </ul>
+                <img src={flagUrl} alt="No flag found" height="250" width="350"/>
+            </div>
+        )
+    }
     return(
         <div>
-            <p>{name}</p>
+            {name}
         </div>
     )
 }
