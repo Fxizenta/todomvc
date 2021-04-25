afd99aa91ea9d751cc8d347ab3fd92f355c6d7db
Fxizenta <1193295835@qq.com>
2021-04-25 20:05:24 +0800

    add new file and finsh 3.1,3.2

--- part3/ex/index.js
+++ part3/ex/index.js
@@ -1,4 +1,7 @@
-const http = require('http')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
 
 let notes = [
     {
@@ -20,11 +23,37 @@ let notes = [
         important: true
     }
 ]
-const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'application/json' })
-    response.end(JSON.stringify(notes))
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
 })
 
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id) //request默认返回的是字符串所以使用number进行转换后再进行比较
+    const note = notes.find(note => note.id === id)
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+    const note = request.body
+    console.log(note)
+    response.json(note)
+})
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
